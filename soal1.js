474aecfb6811ea0321fed00cf5862351866729db
fakeboyas <ovoivi9@gmail.com>
2020-06-11 15:09:18 +0700

    first commit

---
+++ soal1.js
@@ -0,0 +1,9 @@
+let jobDivision = "Akuntan";
+
+if (jobDivision == "HR") {
+  console.log("Tugas saya adalah melakukan rekrutmen untuk calon pegawai");
+} else if (jobDivision == "Akuntan") {
+  console.log("Tugas saya adalah membuat laporan keuangan");
+} else if (jobDivision == "Sales") {
+  console.log("Tugas saya adalah Melakukan penjualan");
+} else console.log("Anda Bukan Karyawan");
---
+++ soal4.js
@@ -0,0 +1,17 @@
+let move = "down";
+
+switch (move) {
+  case "up":
+    console.log("Karakter berjalan ke atas");
+    break;
+  case "right":
+    console.log("Karakter berjalan ke kanan");
+    break;
+  case "left":
+    console.log("Karakter berjalan ke kiri");
+    break;
+  case "down":
+    console.log("Karakter berjalan ke bawah");
+  default:
+    break;
+}
